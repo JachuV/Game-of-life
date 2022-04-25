7f7c2508c214b7c69f373afdaea9252fa331408b
Maks <maks0047@gmail.com>
2022-04-25 14:45:09 +0200

    Game-of-live-02 - add function:changeStatus, addAliveNeighbor, checkNeighbor; modify changeValue, do some refactor

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,32 +11,74 @@ function newBoard (width = 5, height = 5) {
     return generatedBoard;
 }
 
-function changeValue(classList) {
+function changeStatus(board) {
+    let maxHeight = board.length;
+    let maxWidth = board[0].length;
+    for (let width = 0; width < maxWidth; width++){
+        for (let height = 0; height < maxHeight; height++) {
+            board[width][height] = checkNeighbor(board,width,height);
+        }
+    }
+    return board;
+}
+
+function changeValue(classList, board, width, height) {
     if (classList.contains('alive')) {
         classList.remove('alive');
+        board[height][width] = false;
     } else {
         classList.add('alive');
+        board[height][width] = true;
     }
 }
 
-function changeCell(width, height) {
+function changeCell(width, height, board) {
     let cell = document.querySelector(`.W${width}H${height}`);
     cell.addEventListener('click', () => {
-        changeValue(cell.classList);
+        changeValue(cell.classList, board, width, height);
     });
 }
 
 function changeCells(board) {
     for (let h = 0; h < board.length; h++) {
         for (let w = 0; w < board[0].length; w++) {
-            changeCell(w, h);
+            changeCell(w, h, board);
         }
     }
 }
 
-// function countNeighbors() {
-//     if ()
-// }
+function addAliveNeighbor(w, h) {
+
+    try {
+        if (board[w][h]) {
+            console.log(1);
+            return 1;
+        }
+        console.log(0);
+        return 0;
+    } catch {
+        console.log(-1);
+        return 0;
+    }
+}
+
+function checkNeighbor(board, width, height) {
+    let count = 0;
+    const value = board[width][height];
+    count += addAliveNeighbor(width - 1, height - 1);
+    count += addAliveNeighbor(width - 1, height);
+    count += addAliveNeighbor(width - 1, height + 1);
+    count += addAliveNeighbor(width, height - 1);
+    count += addAliveNeighbor(width, height + 1);
+    count += addAliveNeighbor(width + 1, height - 1);
+    count += addAliveNeighbor(width + 1, height);
+    count += addAliveNeighbor(width + 1, height + 1);
+    if (value === true){
+        return (2 <= count && count <= 3);
+    } else {
+        return (count === 3);
+    }
+}
 
 generateCell = (isAlive, width, height) => {
     const cell = document.createElement('div');
@@ -45,7 +87,6 @@ generateCell = (isAlive, width, height) => {
         cell.classList.add('alive');
     }
     return cell;
-
 }
 
 renderBoard = boolBoard => {
@@ -62,20 +103,19 @@ renderBoard = boolBoard => {
         }
         htmlBoard.appendChild(row);
     }
-
     return boardWrapper.appendChild(htmlBoard);
 }
 
 
 
-let counter = 0;
-
-do{
-    counter ++;
-    let board = newBoard(40, 20);
-    renderBoard(board);
-    changeCells(board);
+let board = newBoard(40, 20);
+board[5][5] = true;
+board[5][6] = true;
+board[5][7] = true;
 
 
+renderBoard(board);
+changeCells(board);
+board = changeStatus(board);
+renderBoard(board);
 
-} while (counter === 5)
